8f5f3122b16fa60479cabd5532d94a35b468d4fc
iketata1 <Iketata1@gmail.com>
2026-05-03 14:57:52 +0100

    Fix API routing for Vercel serverless functions

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
 // Vercel Serverless Function wrapper for Express backend
 const app = require('../backend/server');
 
-module.exports = app;
+// Export the Express app as a serverless function
+module.exports = (req, res) => {
+  // Remove /api prefix from the path for Express routing
+  req.url = req.url.replace(/^\/api/, '');
+  
+  // If the URL is empty after removing /api, set it to /
+  if (!req.url || req.url === '') {
+    req.url = '/';
+  }
+  
+  return app(req, res);
+};
+
